8798ca2cfc9eb091e40062cb9222603ffd8f80c1
RutujaRM <96306705+RutujaRM@users.noreply.github.com>
2022-03-12 13:44:03 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+
+
+
+const element = document.getElementById("myBtn");
+element.addEventListener("click", function() 
+{
+  document.getElementById("demo").innerHTML = "<h1>Shopping list</h1><p>A shopping list is a list of items needed to be purchased by a shopper. Consumers often compile a shopping list of groceries to purchase on the next visit to the grocery store (a grocery list). The shopping list was known 2000 years B.C. in ancient Mesopotamia.[1][unreliable source?] There are surviving examples of Roman[2] and Biblical[3] shopping lists.<p> The shopping list itself may be simply a scrap piece of paper or something more elaborate. There are pads with magnets for keeping an incremental list available at the home, typically on the refrigerator, but any magnetic clip with scraps of paper can be used to achieve the same result. There is even a specific device that dispenses a strip of paper from a roll for use in a shopping list. Some shopping carts come with a small clipboard to fit shopping lists on.</p>";
+  
+});
+
+
+
+const element1 = document.getElementById("myBtn1");
+element1.addEventListener( "click", function()
+{
+  document.getElementById("demo1").innerHTML = "<h1>Fusion energy</h1><p> Generating endless energy with zero emissions by just slamming hydrogen atoms together has been somewhat of a pipe dream for decades.Now,scientists may be getting a tiny step closer to feasible fusion power,thanks to a futuristics experiment and dozens of plasma guns.</p>";
+  
+});
+
+
+const element2 = document.getElementById("myBtn2");
+element2.addEventListener( "click", function()
+{
+  document.getElementById("demo2").innerHTML = "<h1>The Types of Design Meetings</h1><p>Certain phrases, when uttered into the void of a Calendar invitation, have very specific meanings to designers.For example, if you wake a lead designer up before 8am for a Design Review, our expectation is that we’ll be leading a call where the work to date is packaged up and presented to directors, VPs, or even a sponsoring executive. We’re probably up and caffeinating 90min beforehand. It isn’t a phrase that you should use lightly when summoning a creative.</p><h3>Intake</h3><p>Introduce the new project, requirements, and expected delivery window to team leads.</p><h3>Unpack</h3><p>After the design lead(s) process the intake it is unpacked to the creative team in an actionable way, typically using a creative brief. This will include a summary of the intake alongside research findings and a design challenge articulation.</p><h3>Alignment</h3><p>An expanded version of the processed intake will be placed into a project brief. The goal is for the lead designer to restate and reframe the intake in an actionable way that everyone can agree on while setting expectations for what will be delivered. Once the agreement is made any shifts in the strategy will affect the delivery timeline.</p><h3>Ideation</h3><p>This is a creative call where we brainstorm potential solutions to a design challenge.</p>";
+  
+});
+
+
+
+
+const element3 = document.getElementById("myBtn3");
+element3.addEventListener( "click", function()
+{
+  document.getElementById("demo3").innerHTML = "<h1>Activation mode</h1><p>Activation mode for an application defines the way an application is loaded, initiated and started when deployed to an AppSpace. By choosing the appropriate activation mode, the component in an application module can be configured to be active in multiple AppNodes or in a single AppNode.</p><p>The Activation Mode option will be available in the preflight stage of the experiment and users can choose the specific mode that suits their necessity.</p><h3>Activate on Page load</h3><p>By default, all the experiments have “Activate on Page Load” as the activation mode, which means, the experiment gets activated on the page load.</p><h3>Activate Manually</h3><p></p>Choosing the Activate Manually mode will generate the web application's JavaScript activation API that will be included in a significant part of your site’s code for experiment activation. <ul><li>Some scenarios where Manual activation mode can come in handy are,</li><li>A web page where the experiment needs to be manually activated at some point of time after the page load.</li><li>You prefer employing manual code to activate the experiment so that it gives you more control over the site.</li></ul>";
+  
+});
+
+
+
+const element5 = document.getElementById("myBtn5");
+element5.addEventListener( "click", function()
+{
+  document.getElementById("demo4").innerHTML = "<h1>Meteorite</h1><p>A meteorite is a solid piece of debris from an object, such as a comet, asteroid, or meteoroid, that originates in outer space and survives its passage through the atmosphere to reach the surface of a planet or moon. When the original object enters the atmosphere, various factors such as friction, pressure, and chemical interactions with the atmospheric gases cause it to heat up and radiate energy. It then becomes a meteor and forms a fireball, also known as a shooting star or falling star; astronomers call the brightest examples. Once it settles on the larger body's surface, the meteor becomes a meteorite. Meteorites vary greatly in size. For geologists, a bolide is a meteorite large enough to create an impact crater.</p><p>Meteorites that are recovered after being observed as they transit the atmosphere and impact the Earth are called meteorite falls. All others are known as meteorite finds. As of August 2018, there were about 1,412 witnessed falls that have specimens in the world's collections.[3] As of July 2021, there are more than 65,780 well-documented meteorite finds.</p><p>Meteorites have traditionally been divided into three broad categories: stony meteorites that are rocks, mainly composed of silicate minerals; iron meteorites that are largely composed of ferronickel; and stony-iron meteorites that contain large amounts of both metallic and rocky material. Modern classification schemes divide meteorites into groups according to their structure, chemical and isotopic composition and mineralogy. Meteorites smaller than 2 mm are classified as micrometeorites. Extraterrestrial meteorites have been found on the Moon and on Mars.</p>";
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+
